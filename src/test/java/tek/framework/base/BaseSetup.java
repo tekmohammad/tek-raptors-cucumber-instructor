be4802563742dec2047674348b847b78558624f2
Mohammad Shokriyan <mohammad@tekschool.us>
2023-12-18 20:55:12 -0500

    add config files

--- src/test/java/tek/framework/base/BaseSetup.java
+++ src/test/java/tek/framework/base/BaseSetup.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseSetup {
 
@@ -15,8 +18,10 @@ public class BaseSetup {
     private static WebDriver driver;
 
     public void setupBrowser() {
+       Properties configs = readProperties();
+
         //Cross Browser Testing
-        String browserType = "chrome";
+        String browserType = configs.getProperty("browser");
         switch (browserType.toLowerCase()) {
             case "chrome":
                 driver = new ChromeDriver();
@@ -33,7 +38,25 @@ public class BaseSetup {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://retail.tekschool-students.com/");
+        String url = configs.getProperty("retail.url");
+        driver.get(url);
+    }
+
+    private Properties readProperties(){
+        //Full file path to the configuration file.
+        //System.getProprty("user.dir") return directory up to project level.
+        String propertyFilePath = System.getProperty("user.dir")
+                + "/src/test/resources/configs/qa-env.properties";
+        //Create and Object of Properties Class in java.util package.
+        Properties properties = new Properties();
+        try {
+            FileInputStream inputStream = new FileInputStream(propertyFilePath);
+            properties.load(inputStream);
+        }catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Can not read property file");
+        }
+        return properties;
     }
 
     //Having only Getter for Encapsulated variable we only give other objects.
